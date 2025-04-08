ba3d670be48f6d0aa76435aa265428296350d920
Prabin Joshi <joshiprabin17@gmail.com>
2025-04-09 01:04:17 +0545

    Refactor Footer and Navbar components, add Contact Us page with form handling and toast notifications

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,37 @@
 import React from "react";
-import { createRoot } from "react-dom/client"; // Updated import
-// import { BrowserRouter as Router } from 'react-router-dom';
+import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./App";
 import "./index.css";
 import Footer_Section from "./components/Footer_Section";
+import ContactUsPage from "./pages/ContactUsPage";
+import { Bounce, ToastContainer } from "react-toastify";
 
 const root = createRoot(document.getElementById("root")); // Create a root
 root.render(
   <BrowserRouter>
+    <ToastContainer
+      position="top-right"
+      autoClose={2000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick={false}
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+      transition={Bounce}
+    />
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/home" element={<App />} />
+      <Route path="/blog" element={<App />} />
+      <Route path="/single-post" element={<App />} />
+      <Route path="/categories" element={<App />} />
+      <Route path="/contact" element={<ContactUsPage />} />
+      <Route path="/account" element={<App />} />
+
     </Routes>
     <Footer_Section />
   </BrowserRouter>
