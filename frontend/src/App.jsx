07a015e53a878f6b5c5ea41a66f03025f7dc3158
Ritesh Tamang <riteshtmglama001@gmail.com>
2025-04-09 21:12:37 +0545

    complete social media game section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import FirstNavbar from './components/FirstNavbar'
+import SocialMediaGame from './components/SocialMediaGame'
 
 export default function App() {
   return (
     <div className=''>
       {/* <FirstNavbar></FirstNavbar> */}
+     <SocialMediaGame/>
     </div>
   )
 }
