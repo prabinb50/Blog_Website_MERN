617fb44df8d67d4a34d5f2b8e977b24cab383269
Ritesh Tamang <riteshtmglama001@gmail.com>
2025-04-12 20:54:06 +0545

    complete home page section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import FirstNavbar from './components/FirstNavbar'
 import HeroSection from './components/HeroSection'
 import NewFeedSection from './components/newsFeedSectio/NewsFeedSection'
+import BlogPostCategories from './components/BlogPostCategories'
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       {/* <FirstNavbar></FirstNavbar> */}
       <HeroSection/>
       <NewFeedSection/>
+      <BlogPostCategories/>
     </div>
   )
 }
