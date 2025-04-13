30a57e21203aff810ab4a33b0479310276f6885b
Prabin Joshi <joshiprabin17@gmail.com>
2025-04-13 16:36:36 +0545

    Update dependencies, refactor login and signup pages, and remove unused footer section

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router";
 import App from "./App";
 import "./index.css";
 import Footer_Section from "./components/Footer_Section";
@@ -11,43 +11,57 @@ import BlogPage from "./pages/BlogPage";
 import SecondNavbar from "./components/SecondNavbar";
 import CategoriesPage from "./pages/CategoriesPage";
 import ScrollToTopArrow from "./components/ScrollToTopArrow";
-import SocialMediaGame from './components/SocialMediaGame'
+import SocialMediaGame from './components/SocialMediaGame';
 import AccountPage from "./pages/AccountPage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 
+function AppWrapper() {
+  const location = useLocation(); // Get the current route
+
+  return (
+    <>
+      <SecondNavbar />
+
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+      />
+
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/single-post" element={<SinglePostPage />} />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/categories" element={<CategoriesPage />} />
+        <Route path="/contact" element={<ContactUsPage />} />
+        <Route path="/account" element={<AccountPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+
+      {/* Conditionally render SocialMediaGame */}
+      {location.pathname !== "/sign-up" && location.pathname !== "/login" && (
+        <SocialMediaGame />
+      )}
+
+      <Footer_Section />
+      <ScrollToTopArrow />
+    </>
+  );
+}
+
 const root = createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <SecondNavbar />
-
-    <ToastContainer
-      position="top-right"
-      autoClose={2000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick={false}
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-      theme="light"
-      transition={Bounce}
-    />
-
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/single-post" element={<SinglePostPage />} />
-      <Route path="/blog" element={<BlogPage />} />
-      <Route path="/categories" element={<CategoriesPage />} />
-      <Route path="/contact" element={<ContactUsPage />} />
-      <Route path="/account" element={<AccountPage />} />
-      <Route path="/sign-up" element={<SignUpPage />} />
-      <Route path="/login" element={<LoginPage />} />
-    </Routes>
-
-    <SocialMediaGame />
-    <Footer_Section />
-    <ScrollToTopArrow />
+    <AppWrapper />
   </BrowserRouter>
 );
