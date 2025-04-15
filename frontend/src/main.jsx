5d98ca1532d9ece3824812c74c8ea0726fc36e21
Prabin Joshi <joshiprabin17@gmail.com>
2025-04-15 18:49:02 +0545

    Refactor BlogPageOne component layout and remove commented-out code in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -61,39 +61,7 @@ function AppWrapper() {
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-{/*  */}
-    <SecondNavbar />
-
-    <ToastContainer
-      position="top-right"
-      autoClose={2000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick={false}
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-      theme="light"
-      transition={Bounce}
-    />
-
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/single-post" element={<SinglePostPage />} />
-      <Route path="/blog" element={<BlogPage />} />
-      <Route path="/categories" element={<CategoriesPage />} />
-      <Route path="/contact" element={<ContactUsPage />} />
-      <Route path="/account" element={<AccountPage />} />
-      <Route path="/sign-up" element={<SignUpPage />} />
-      <Route path="/login" element={<LoginPage />} />
-    </Routes>
-
-    <SocialMediaGame />
-    <FooterSection />
-    <ScrollToTopArrow />
+  <BrowserRouter >
     <AppWrapper />
-
-  </BrowserRouter>
+  </BrowserRouter >
 );
