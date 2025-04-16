bab167751a8cfae08b901ea467a48ed81382b0ca
Ritesh Tamang <riteshtmglama001@gmail.com>
2025-04-16 17:07:47 +0545

    remove social network from mobile menu navbar and rearranged socialmedia game file

--- frontend/src/components/SearchSection.jsx
+++ frontend/src/components/SearchSection.jsx
@@ -1,11 +1,6 @@
 import * as React from "react";
 import Box from "@mui/joy/Box";
 import Drawer from "@mui/joy/Drawer";
-import Button from "@mui/joy/Button";
-import List from "@mui/joy/List";
-import Divider from "@mui/joy/Divider";
-import ListItem from "@mui/joy/ListItem";
-import ListItemButton from "@mui/joy/ListItemButton";
 import { Search } from "lucide-react";
 
 export default function SearchSection() {
@@ -34,18 +29,17 @@ export default function SearchSection() {
         />
       </button>
       <Drawer anchor="top" open={open} onClose={toggleDrawer(false)}>
-        <Box
-       
-          
-        >
-            <div className="flex flex-col items-center justify-center w-full mt-24 gap-8">
-                <p className="md:text-4xl text-3xl font-semibold">You Can Easily Search Here.</p>
-                <input  type="text" className="outline-none border-b border-gray-300 px-4 py-2 font-semibold text-lg w-88 md:w-100" placeholder="Search..."/>
-            </div>
-
- 
-
-
+        <Box>
+          <div className="flex flex-col items-center justify-center w-full mt-24 gap-8">
+            <p className="md:text-4xl text-3xl font-semibold">
+              You Can Easily Search Here.
+            </p>
+            <input
+              type="text"
+              className="outline-none border-b border-gray-300 px-4 py-2 font-semibold text-lg w-88 md:w-100 tracking-wider"
+              placeholder="Search..."
+            />
+          </div>
         </Box>
       </Drawer>
     </Box>
