6de52bd4c3af4c4ff67c2039f6560f8d5d6b3a63
Ritesh Tamang <riteshtmglama001@gmail.com>
2025-04-17 14:51:12 +0545

    some files style changes

--- frontend/src/components/SecondNavbar.jsx
+++ frontend/src/components/SecondNavbar.jsx
@@ -92,7 +92,7 @@ export default function SecondNavbar() {
             {/* Subscribe button */}
             <Link
               to={"/sign-up"}
-              className="bg-purple-800 text-white font-semibold px-4 py-3 rounded-full hover:bg-black transition duration-300 ease-in-out cursor-pointer"
+              className="bg-purple-800 text-white font-semibold px-4 md:py-3 py-2 rounded-full hover:bg-black transition duration-300 ease-in-out cursor-pointer"
             >
               Subscribe
             </Link>
--- frontend/src/components/SocialMediaGame.jsx
+++ frontend/src/components/SocialMediaGame.jsx
@@ -6,10 +6,10 @@ export default function SocialMediaGame() {
     <div className="h-full lg:h-120 w-full bg-violet-900  flex ">
       <div className=" w-11/12 mx-auto flex flex-col max-lg:px-6 lg:flex-row items-center justify-between gap-8">
         <div className="flex flex-col  lg:w-1/2  gap-8 max-lg:mt-12">
-          <p className="font-semibold md:text-6xl text-5xl text-white opacity-90 leading-16 lg:leading-18">
+          <p className="font-bold xl:text-6xl sm:text-4xl xl:leading-16 text-3xl text-white  opacity-80">
             Ready to Elevate Your Social Media Game?
           </p>
-          <p className="text-white opacity-80 text-lg tracking-wide leading-8">
+          <p className="text-white opacity-80 md::text-lg tracking-wide leading-8">
             Unlock the tools and insights you need to thrive on social media
             with Vexon. Join our community for expert tips, trending strategies,
             and resources that empower you to stand out and succeed.
@@ -17,11 +17,11 @@ export default function SocialMediaGame() {
           <div className=" flex flex-col relative  justify-center  ">
             <input
               type="text"
-              className="outline-none  h-16 flex items-center px-4 py-2 bg-white rounded-4xl "
+              className="outline-none  md:h-16 h-14 flex items-center px-4 py-2 bg-white rounded-4xl "
               placeholder="Enter Your Email"
             />
 
-            <button className="  absolute flex items-center  right-2 cursor-pointer hover:bg-black duration-500  px-6 py-3 font-bold bg-violet-800 text-white rounded-4xl">
+            <button className="  absolute flex items-center  right-2 cursor-pointer hover:bg-black duration-500 px-4 py-2  md:px-6 md:py-3 font-bold bg-violet-800 text-white rounded-4xl">
               Get Start
             </button>
           </div>
