0f3a781ff33147fdcbdd2b2826225e21500f0f07
Prabin Joshi <joshiprabin17@gmail.com>
2025-04-18 20:44:11 +0545

    create schema for blogSingle

---
+++ backend/schema/BlogSingle.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+// Define the schema for the blog single page
+const blogSingleSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    image: { type: String, required: true },
+    date: { type: Date, default: Date.now },
+    author: { type: String, required: true },
+    readTime: { type: String },
+});
+
+// Create the model (table) for the blog single page
+export const BlogSingle = mongoose.model("BlogSingle", blogSingleSchema);
