dc933998ddf182607ef16ba29eab27f52cd5328f
Prabin Joshi <joshiprabin17@gmail.com>
2025-04-27 21:52:05 +0545

    Refactor ProtectedRoute component and update AppWrapper to improve authentication handling and code readability

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Navigate } from 'react-router';
 
 const ProtectedRoute = ({ children }) => {
-    // Check if the user is authenticated
-    const isAuthenticated = localStorage.getItem('myToken');
+    // check if token is present in local storage
+    const isAuthenticated = localStorage.getItem('myToken');  
 
     // If the user is authenticated, render the children (protected component), otherwise redirect to login page
     return isAuthenticated ? children : <Navigate to="/login" />;
