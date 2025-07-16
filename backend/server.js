e2c17d2ff4b67f53655053eacc80196ed5655ac3
Prabin Joshi <joshiprabin17@gmail.com>
2025-07-16 16:00:46 +0545

    updated

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(cors({
 
 // connect to the database
 try {
-    mongoose.connect("mongodb+srv://joshiprabin17:g0HV1i3Be2XrIYD8@cluster0.kpv00gt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB');
 } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
