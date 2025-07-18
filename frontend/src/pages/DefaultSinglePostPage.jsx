f91516b6c343ab1fffac96101420f4cd2de31820
Prabin Joshi <joshiprabin17@gmail.com>
2025-07-18 15:49:19 +0545

    add scroll based animations in single post page

--- frontend/src/pages/DefaultSinglePostPage.jsx
+++ frontend/src/pages/DefaultSinglePostPage.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { FaFacebookF, FaInstagram, FaLinkedinIn } from "react-icons/fa";
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink } from "react-router";
 import { ChevronRight } from "lucide-react";
 import { Bounce, toast } from "react-toastify";
 import MoreBlogsSinglePost from "../components/moreBlogsSinglePost";
+import { AnimatedText, AnimatedCard, AnimatedFade } from "../components/AnimatedComponent";
 
 export default function DefaultSinglePostPage() {
-
-  // State variables for form inputs
+  // state variables for form inputs
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
@@ -15,10 +15,11 @@ export default function DefaultSinglePostPage() {
   const [subject, setSubject] = useState("");
   const [phone, setPhone] = useState("");
 
-  // Function to handle form submission
+  // function to handle form submission
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Display a toast notification
+
+    // display a toast notification
     toast("Thank you for your message! We will get back to you soon.", {
       position: "top-right",
       autoClose: 2000,
@@ -30,7 +31,7 @@ export default function DefaultSinglePostPage() {
       theme: "light",
       transition: Bounce,
     });
-    // Reset form fields
+    // reset form fields
     setFirstName("");
     setLastName("");
     setEmail("");
@@ -41,31 +42,33 @@ export default function DefaultSinglePostPage() {
 
   return (
     <div className="bg-gray-50 min-h-screen">
-      {/* Light purple background for title section */}
       <div className="bg-[#f6f2ff] w-full">
-        <div className="flex items-center justify-center pt-20 pb-2 overflow-x-auto px-4">
-          {/* Breadcrumb navigation */}
+        <AnimatedFade className="flex items-center justify-center pt-20 pb-2 overflow-x-auto px-4" delay={0.1}>
           <NavLink to={"/"} className="cursor-pointer ">
             Home
           </NavLink>
-          <ChevronRight className="w-5" />
-          <p className="whitespace-nowrap ">Blog</p>
-          <ChevronRight className="w-5" />
-          <p className="font-bold whitespace-nowrap">Blog Without Sidebar</p>
-        </div>
 
-        {/* Page title */}
-        <p className="text-center font-bold text-4xl md:text-5xl pb-20 px-4">
+          <ChevronRight className="w-5" />
+
+          <p className="whitespace-nowrap ">Blog</p>
+
+          <ChevronRight className="w-5" />
+
+          <p className="font-bold whitespace-nowrap">Blog Without Sidebar</p>
+        </AnimatedFade>
+
+        {/* page title */}
+        <AnimatedText className="text-center font-bold text-4xl md:text-5xl pb-20 px-4" delay={0.2}>
           Blog Without Sidebar
-        </p>
+        </AnimatedText>
       </div>
 
-      {/* Main Content Section */}
-      <div className="w-[90%] sm:w-11/12 lg:w-9/12 xl:w-7/12 mx-auto pt-15 md:pt-20 pb-15 pb:pb-20">
-        {/* Post Header */}
-        <header className="flex flex-col sm:flex-row justify-between items-start sm:items-center mb-4 sm:mb-6">
-          <div className="flex items-center mb-3 sm:mb-0">
-            {/* Author profile image and name */}
+      {/* main content section */}
+      <div className="w-[90%] sm:w-11/12 lg:w-9/12 xl:w-7/12 mx-auto pt-13 sm:pt-16 md:pt-20 pb-13 sm:pb-16 pb:pb-20">
+        {/* post header */}
+        <AnimatedFade className="flex justify-between items-center mb-4 sm:mb-6" delay={0.3}>
+          <div className="flex items-center ">
+            {/* author profile image and name */}
             <img
               src="/blog1-author1.png"
               alt="Author"
@@ -73,37 +76,45 @@ export default function DefaultSinglePostPage() {
             />
 
             <span className="font-semibold text-gray-800 text-sm sm:text-base">
-              Kimberly Mastrangelo
+              Prabin Joshi
             </span>
           </div>
 
-          <div className="text-xs sm:text-sm text-gray-500">
-            {/* Post date and read time */}
+          <div className="text-sm sm:text-base text-gray-500 ">
+            {/* post date and read time */}
             <span>Oct 26, 2025</span>
 
-            <span className="mx-2 inline">|</span>
+            <span className="mx-1 inline ">|</span>
 
             <span>3 min read</span>
           </div>
-        </header>
+        </AnimatedFade>
 
-        {/* Post Content */}
-        <div className="mb-6 grid grid-cols-1 gap-3 sm:gap-4">
-          {/* Post title */}
-          <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold text-gray-900 mb-2 sm:mb-4">
-            The Art of Building a Strong Personal Brand on Social Media
+        {/* post content */}
+        <AnimatedCard className="mb-6 grid grid-cols-1 gap-3 sm:gap-4" delay={0.4}>
+          {/* post title */}
+          <h1 className="text-2xl sm:text-3xl md:text-4xl font-semibold text-gray-900 mb-2 sm:mb-4 text-center">
+            <span className="inline sm:inline-block sm:w-full mb-2">The Art of Building a Strong Personal Brand</span>
+
+            <span className="inline sm:inline-block sm:w-full"> on Social Media</span>
           </h1>
 
-          {/* Post description */}
-          <p className="text-sm sm:text-base text-gray-700 leading-relaxed">
-            In today’s digital age, building a strong personal brand on social
+          {/* post description */}
+          {/* <p className="text-sm sm:text-base text-gray-700 leading-relaxed">
+            In today's digital age, building a strong personal brand on social
             media is more crucial than ever. A compelling personal brand not
-            only establishes your credibility and trustworthiness but also opens
-            doors to new opportunities, connects you with like-minded
-            individuals, ...
+            only establishes your credibility and trustworthiness but also opens doors to new opportunities, connects you with like-minded individuals,
+          </p> */}
+
+          <p className="text-sm sm:text-base text-gray-700 leading-relaxed text-center">
+            <span className="inline sm:inline-block sm:w-full">In today's digital age, building a strong personal brand on social
+              media is more crucial than ever. A compelling personal brand not
+              only establishes your credibility and trustworthiness but also opens</span>
+
+            <span className="inline sm:inline-block sm:w-full"> doors to new opportunities, connects you with like-minded individuals,</span>
           </p>
 
-          {/* Post image */}
+          {/* post image */}
           <div className="my-2 sm:my-4">
             <img
               src="/blog-details-image1.png"
@@ -112,7 +123,7 @@ export default function DefaultSinglePostPage() {
             />
           </div>
 
-          {/* Additional content */}
+          {/* additional content */}
           <p className="text-sm sm:text-base text-gray-700 leading-relaxed">
             In today's digital age, building a strong personal brand on social
             media is more crucial than ever. A compelling personal brand not
@@ -123,89 +134,108 @@ export default function DefaultSinglePostPage() {
             seem daunting, but with the right strategies, you can create a brand
             that resonates.
           </p>
-        </div>
+        </AnimatedCard>
 
-        {/* Additional Sections */}
-        <h2 className="text-xl sm:text-2xl md:text-3xl font-semibold mt-6 mb-3">
+        {/* section heading */}
+        <AnimatedText className="text-xl sm:text-2xl md:text-3xl font-semibold mt-6 mb-3" delay={0.5}>
           Understanding Your Personal Brand
-        </h2>
+        </AnimatedText>
 
-        <p className="text-sm sm:text-base text-gray-700 leading-relaxed">
+        {/* section content */}
+        <AnimatedText className="text-sm sm:text-base text-gray-700 leading-relaxed" delay={0.5}>
           Your personal brand is a reflection of who you are, what you stand
           for, and how you want others to perceive you. Before diving into the
           social media world, take some time to reflect on your values,
           interests, and goals. Ask yourself: What do I want to be known for?
           What are my unique strengths and passions? Who is my target audience?
-        </p>
+        </AnimatedText>
 
-        {/* Responsive Images */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-3 sm:gap-4 mt-4 sm:mt-6">
+        {/* images  */}
+        <AnimatedCard className="grid grid-cols-1 sm:grid-cols-2 gap-3 sm:gap-4 mt-4 sm:mt-6" delay={0.6}>
           <img
             src="/blog-details-image2.png"
             alt=""
             className="rounded-lg object-cover w-full h-auto"
           />
+
           <img
             src="/blog-details-image3.png"
             alt=""
             className="rounded-lg object-cover w-full h-auto "
           />
-        </div>
+        </AnimatedCard>
 
-        {/* Quote Section */}
-        <div className="gap-3 grid grid-cols-1 border border-gray-300 rounded-md bg-purple-800 mt-4 sm:mt-6 p-4 sm:p-6">
+        {/* quote section  */}
+        <AnimatedCard
+          className="gap-3 grid grid-cols-1 border border-gray-300 rounded-md bg-purple-800 mt-4 sm:mt-6 p-4 sm:p-6"
+          delay={0.7}>
           <span className="text-white text-base sm:text-lg md:text-xl lg:text-2xl font-semibold">
             <q>
               Your personal brand is the unique story that only you can tell.
               Own it, share it, and let it shine.
             </q>
           </span>
-          <span className="text-white text-sm sm:text-base">Henry Fawyel</span>
-        </div>
 
-        {/* Tags and Socials */}
-        <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between py-4 sm:py-6 md:py-8 gap-4">
+          <span className="text-white text-sm sm:text-base">Henry Fawyel</span>
+        </AnimatedCard>
+
+        {/* tags and socials */}
+        <AnimatedFade
+          className="flex flex-col sm:flex-row items-start sm:items-center justify-between py-4 sm:py-6 md:py-8 gap-4"
+          delay={0.8}
+        >
           <div className="flex flex-wrap items-center gap-2 sm:gap-3">
-            {/* Tags */}
+            {/* tags */}
             <p className="font-semibold text-base sm:text-lg md:text-xl lg:text-2xl">Tags:</p>
-            <div className="text-xs sm:text-sm text-gray-500 hover:text-white border border-gray-500 cursor-pointer w-max p-1.5 sm:p-2 rounded-full hover:bg-violet-600 hover:border-violet-600 duration-700">
+
+            <div className="text-sm sm:text-base text-gray-500 hover:text-white border border-gray-500 cursor-pointer w-max p-1.5 sm:p-2 rounded-full hover:bg-violet-600 hover:border-violet-600 duration-700">
               Social Media
             </div>
-            <div className="text-xs sm:text-sm text-gray-500 hover:text-white border border-gray-500 cursor-pointer w-max p-1.5 sm:p-2 rounded-full hover:bg-violet-600 hover:border-violet-600 duration-700">
+
+            <div className="text-sm sm:text-base text-gray-500 hover:text-white border border-gray-500 cursor-pointer w-max p-1.5 sm:p-2 rounded-full hover:bg-violet-600 hover:border-violet-600 duration-700">
               UI/UX
             </div>
-            <div className="text-xs sm:text-sm text-gray-500 hover:text-white border border-gray-500 cursor-pointer w-max p-1.5 sm:p-2 rounded-full hover:bg-violet-600 hover:border-violet-600 duration-700">
+
+            <div className="text-sm sm:text-base text-gray-500 hover:text-white border border-gray-500 cursor-pointer w-max p-1.5 sm:p-2 rounded-full hover:bg-violet-600 hover:border-violet-600 duration-700">
               Business
             </div>
           </div>
 
           <div className="flex items-center gap-2 sm:gap-3 mt-3 sm:mt-0">
-            {/* Social media icons */}
+            {/* social media icons */}
             <p className="text-base sm:text-lg md:text-xl lg:text-2xl font-semibold">Socials:</p>
+
             <FaFacebookF
               size={30}
               className="text-gray-500 hover:text-white border border-gray-500 cursor-pointer w-max p-1.5 sm:p-2 rounded-full hover:bg-violet-600 hover:border-violet-600 duration-700"
             />
+
             <FaInstagram
               size={30}
               className="text-gray-500 hover:text-white border border-gray-500 cursor-pointer w-max p-1.5 sm:p-2 rounded-full hover:bg-violet-600 hover:border-violet-600 duration-700"
             />
+
             <FaLinkedinIn
               size={30}
               className="text-gray-500 hover:text-white border border-gray-500 cursor-pointer w-max p-1.5 sm:p-2 rounded-full hover:bg-violet-600 hover:border-violet-600 duration-700"
             />
           </div>
-        </div>
+        </AnimatedFade>
 
-        {/* Comment Section */}
-        <div className="bg-gray-100 p-4 sm:p-5 md:p-6 rounded-lg shadow-md">
+        {/* comment section with card animation */}
+        <AnimatedCard
+          className="bg-gray-100 p-4 sm:p-5 md:p-6 rounded-lg shadow-md"
+          delay={0.9}
+        >
           <h3 className="text-lg sm:text-xl md:text-2xl font-bold mb-1 sm:mb-2">Leave a Reply</h3>
-          <p className="text-xs sm:text-sm text-gray-600 mb-4 sm:mb-6">
+
+          <p className="text-sm sm:text-base text-gray-600 mb-4 sm:mb-6">
             Provide clear contact information, including phone number, email,
             and address.
           </p>
+
           <form onSubmit={handleSubmit} className="space-y-4 sm:space-y-6">
-            {/* Form fields */}
+            {/* form fields */}
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-6">
               <input
                 value={firstName}
@@ -213,17 +243,19 @@ export default function DefaultSinglePostPage() {
                 required
                 type="text"
                 placeholder="First Name"
-                className="w-full p-2.5 sm:p-3 border border-gray-300 rounded-lg text-sm"
+                className="w-full p-2.5 sm:p-3 border border-gray-300 rounded-lg text-sm sm:text-base"
               />
+
               <input
                 value={lastName}
                 onChange={(e) => setLastName(e.target.value)}
                 required
                 type="text"
                 placeholder="Last Name"
-                className="w-full p-2.5 sm:p-3 border border-gray-300 rounded-lg text-sm"
+                className="w-full p-2.5 sm:p-3 border border-gray-300 rounded-lg text-sm sm:text-base"
               />
             </div>
+
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-6">
               <input
                 value={email}
@@ -231,33 +263,37 @@ export default function DefaultSinglePostPage() {
                 required
                 type="email"
                 placeholder="Email"
-                className="w-full p-2.5 sm:p-3 border border-gray-300 rounded-lg text-sm"
+                className="w-full p-2.5 sm:p-3 border border-gray-300 rounded-lg text-sm sm:text-base"
               />
+
               <input
                 value={phone}
                 onChange={(e) => setPhone(e.target.value)}
                 required
                 type="text"
                 placeholder="Phone"
-                className="w-full p-2.5 sm:p-3 border border-gray-300 rounded-lg text-sm"
+                className="w-full p-2.5 sm:p-3 border border-gray-300 rounded-lg text-sm sm:text-base"
               />
             </div>
+
             <input
               value={subject}
               onChange={(e) => setSubject(e.target.value)}
               required
               type="text"
               placeholder="Subject"
-              className="w-full p-2.5 sm:p-3 border border-gray-300 rounded-lg text-sm"
+              className="w-full p-2.5 sm:p-3 border border-gray-300 rounded-lg text-sm sm:text-base"
             />
+
             <textarea
               value={message}
               onChange={(e) => setMessage(e.target.value)}
               required
               placeholder="Message"
               rows="4"
-              className="w-full p-2.5 sm:p-3 border border-gray-300 rounded-lg text-sm"
+              className="w-full p-2.5 sm:p-3 border border-gray-300 rounded-lg text-sm sm:text-base"
             ></textarea>
+
             <button
               type="submit"
               className="w-full sm:w-auto px-6 bg-purple-600 text-white py-2.5 rounded-lg font-bold hover:bg-purple-700 transition text-sm sm:text-base"
@@ -265,11 +301,11 @@ export default function DefaultSinglePostPage() {
               Get Started
             </button>
           </form>
-        </div>
+        </AnimatedCard>
       </div>
 
-      {/* More Blogs Section */}
+      {/* more blogs section */}
       <MoreBlogsSinglePost />
-    </div>
+    </div >
   );
 }
