d6b0db0311971fe079657e12c0804f4500d2a5c9
Prabin Joshi <joshiprabin17@gmail.com>
2025-08-12 12:11:34 +0545

    refactor: update JWT secret handling and improve MongoDB connection logic

--- backend/server.js
+++ backend/server.js
@@ -17,25 +17,25 @@ app.use(express.json());
 app.use(cors({
     origin: [
         'http://localhost:5173',
-        "https://blog-website-mern-sxag.vercel.app",
+        // "https://blog-website-mern-sxag.vercel.app",
         "https://blog-website-mern-gray.vercel.app",
     ],
 }));
 
 // connect to the database
-// try {
-//     mongoose.connect(process.env.MONGO_URI);
-//     console.log('Connected to MongoDB');
-// } catch (error) {
-//     console.error('Error connecting to MongoDB:', error.message);
-// }
-
 try {
-    mongoose.connect("mongodb+srv://joshiprabin17:g0HV1i3Be2XrIYD8@cluster0.kpv00gt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB');
 } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
 }
+
+// try {
+//     mongoose.connect("mongodb+srv://joshiprabin17:g0HV1i3Be2XrIYD8@cluster0.kpv00gt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+//     console.log('Connected to MongoDB');
+// } catch (error) {
+//     console.error('Error connecting to MongoDB:', error.message);
+// }
 
 app.use("/users", userRoute);
 app.use("/blogs", BlogRoute);
@@ -43,13 +43,13 @@ app.use("/categories", CategoryRoute);
 app.use("/posts", PostsRoute)
 
 // define the port
-// app.listen(process.env.APP_PORT, () => {
-//     console.log(`Server is running on port ${process.env.APP_PORT}`);
-// });
-
-app.listen(4000, () => {
-    console.log(`Server is running on port 4000`);
+app.listen(process.env.APP_PORT, () => {
+    console.log(`Server is running on port ${process.env.APP_PORT}`);
 });
+
+// app.listen(4000, () => {
+//     console.log(`Server is running on port 4000`);
+// });
 
 app.get("/", (req, res) => {
     res.send("Server is working");
